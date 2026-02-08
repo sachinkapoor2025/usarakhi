eae0591af00e7cfe48dab88597eb5db23bf20bb0
mydgv <dgv@mydgv.com>
2026-02-08 12:31:38 +0530

    resolve export error

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -8,7 +8,6 @@ const nextConfig = {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'https://qd8hw52qw2.execute-api.us-east-1.amazonaws.com/feature-initial-code/',
     NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY: process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
   },
-  output: 'export',
 }
 
 module.exports = nextConfig
